7e25fb44388633e2703edece575a1aa91a00ba8e
Anton Shyrokykh <anton@zwapgrid.com>
2019-09-15 06:50:14 +0000

    add web solution, add swagger settings

---
+++ App.Web/Startup.Swagger.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace App.Web
+{
+    // TODO write description
+    public partial class Startup
+    {
+        void ConfigureSwaggerServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "App", Version = "v1" });
+            });
+        }
+
+        void ConfigureSwagger(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "App V1");
+            });
+        }
+    }
+}
---
+++ App.Web/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace App.Web
+{
+    // TODO write description
+    public partial class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // ## ASP NET Core template code
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            // ##
+
+            // ## Code added to the template
+            ConfigureSwaggerServices(services);
+            // ##
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // ## ASP NET Core template code
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // TODO check what it is
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            // ##
+
+            // ## Code added to the template
+            ConfigureSwagger(app, env);
+            // ##
+        }
+    }
+}
