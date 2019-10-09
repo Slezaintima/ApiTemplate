afd1d8cede2d9e98e37377f97fd6c490f4312265
valentajn <V0102@i.ua>
2019-10-09 21:39:51 +0300

    Revert "Revert "added customers project""
    This reverts commit 62e82169d63926ee9c2d28414875bd3bda5bc12e.

--- App.Common/Repositories/ICustomersRepository.cs
+++ App.Common/Repositories/ICustomersRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace App.Repositories
+﻿using System.Collections.Generic;
+
+namespace App.Repositories
 {
     public interface ICustomersRepository
     {
+        int Size { get; }
+        IEnumerable<string> GetCustomers();
+        string GetCustomer(int id);
+        void Add(string customers);
+        void Update(int i, string newCustomer);
     }
 }
