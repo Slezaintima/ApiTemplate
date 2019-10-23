52564345f426c8b3483a23f76e5a1f27dc2f7247
Your Name <you@example.com>
2019-10-23 22:28:57 +0300

    Implement class manager for Goods

--- App.Goods/GoodsManager.cs
+++ App.Goods/GoodsManager.cs
@@ -1,14 +1,33 @@
-﻿using System;
+﻿using App.Models;
+using App.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace App.Goods
 {
     public interface IGoodsManager
-    {
-    
+    { 
+        IEnumerable<Good> GetGoods();
+        Good GetGood(int id);
     }
     public class GoodsManager:IGoodsManager
     {
+        readonly IGodsRepository _repository;
+        public GoodsManager(IGodsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IEnumerable<Good> GetGoods()
+        {
+            return _repository.GetGoods();
+        }
+
+        public Good GetGood(int id)
+        {
+            return _repository.GetGood(id);
+        }
+
     }
 }
