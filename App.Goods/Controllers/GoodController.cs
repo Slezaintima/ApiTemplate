c93871ad35287edc1b594d91f459eb7fb1316eb0
Your Name <you@example.com>
2019-10-23 22:44:18 +0300

    Add controller for Goods

---
+++ App.Goods/Controllers/GoodController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using App.Models;
+
+namespace App.Goods.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class GoodController: ControllerBase
+    {
+        readonly IGoodsManager _goodsManager;
+
+        public GoodController(IGoodsManager goodsManager)
+        {
+
+            _goodsManager = goodsManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Good>> Get()
+        {
+
+            if (_goodsManager.GetGoods() == null)
+                return NotFound();
+            else
+            {
+                var serviceCallResult = _goodsManager.GetGoods().ToList();
+                return serviceCallResult;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Good> GetById(int id)
+        {
+
+            var serviceCallResult = _goodsManager.GetGood(id);
+            return serviceCallResult;
+        }
+    }
+}
