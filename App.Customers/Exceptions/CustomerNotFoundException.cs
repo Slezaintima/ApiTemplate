1414e223b35797fbceacc38ed5bca070e6cbdc3f
valentajn <V0102@i.ua>
2019-11-05 21:51:17 +0200

    Added exceptions

---
+++ App.Customers/Exceptions/CustomerNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using App.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Customers.Exceptions
+{
+    public class CustomerNotFoundException : Exception
+    {
+        public Customer Customer { get; private set; }
+        public CustomerNotFoundException(Customer customer)
+        {
+            Customer = customer;
+        }
+    }
+}
