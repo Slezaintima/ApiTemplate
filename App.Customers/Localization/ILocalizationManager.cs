74735e9daeadb57d66a44a2cf7a467ddb0c9b46f
valentajn <V0102@i.ua>
2019-12-08 23:10:03 +0200

    Added ILocalizationManager

---
+++ App.Customers/Localization/ILocalizationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace App.Customers.Localization
+{
+    public interface ILocalizationManager
+    {
+        string GetResource(string key);
+        string GetResource(string key, CultureInfo culture);
+
+    }
+}
