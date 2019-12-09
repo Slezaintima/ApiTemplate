f0fdccb80c85bf514e44b134b826e4cb9ae912df
Your Name <you@example.com>
2019-12-09 22:42:11 +0200

    Added OrderRepository

---
+++ App.Goods/Repositories/EfOrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using App.Configuration;
+using App.Goods.Database;
+using App.Models;
+using App.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Goods.Repositories
+{
+    public class EfOrderRepository : ITransientDependency, IOrderRepository
+    {
+        private readonly GoodsDbContext _dbContext;
+        public EfOrderRepository(GoodsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void CreateOrder(Order order)
+        {
+            _dbContext.Orders.Add(order);
+            _dbContext.SaveChanges();
+        }
+    }
+}
