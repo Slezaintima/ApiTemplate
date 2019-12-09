4ff90157077e1dc0e7cf901004ad9d4bc80792a3
Your Name <you@example.com>
2019-12-09 22:18:06 +0200

    Added DbContext

---
+++ App.Goods/Database/GoodsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Goods.Database
+{
+    public class GoodsDbContext: DbContext
+    {
+        public DbSet<Good> Goods { get; set; }
+        public GoodsDbContext(DbContextOptions<GoodsDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Good>()
+                .HasKey(g => g.Id);
+        }
+    }
+}
