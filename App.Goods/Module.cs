928a65a246bfe67de80b55b75fced80caed6b025
Your Name <you@example.com>
2019-12-09 22:29:52 +0200

    Added registration for DbContext

--- App.Goods/Module.cs
+++ App.Goods/Module.cs
@@ -6,6 +6,9 @@ using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using App.Repositories;
 using App.Goods.Repositories;
+using App.Goods.Database;
+using Microsoft.EntityFrameworkCore;
+using App.Models;
 
 namespace App.Goods
 {
@@ -15,11 +18,37 @@ namespace App.Goods
         {
             // example of manually registered components
             container.Register(Component.For<IGoodsManager>().ImplementedBy<GoodsManager>().LifestyleTransient());
-            container.Register(Component.For<IGodsRepository>().
-                ImplementedBy<GoodsRepository>().LifestyleTransient());
             container.Register(Component.For<IOrderManager>().ImplementedBy<OrderManager>().LifestyleTransient());
-            container.Register(Component.For<IOrderRepository>().
-                ImplementedBy<OrderRepository>().LifestyleTransient());
+            RegisterDbContext(container);
+        }
+
+        private void RegisterDbContext(IWindsorContainer container)
+        {
+            container.Register(Component.For<DbContextOptions<GoodsDbContext>>().UsingFactoryMethod(() =>
+            {
+                var builder = new DbContextOptionsBuilder<GoodsDbContext>();
+                builder.UseInMemoryDatabase("GoodsDb");
+                return builder.Options;
+            }).LifestyleTransient());
+
+            container.Register(Component.For<GoodsDbContext>().LifestyleTransient());
+
+            InitializeDbContext(container);
+        }
+
+        private void InitializeDbContext(IWindsorContainer container)
+        {
+            using (var context = container.Resolve<GoodsDbContext>())
+            {
+                context.Goods.AddRange(new[]
+                {
+                    new Good { Id = 1,Name = "Kaptsi", Price = 20 },
+                    new Good { Id = 2,Name = "Sapogi", Price = 25},
+                    new Good { Id = 3,Name = "Kufajka", Price = 2}
+                });
+
+                context.SaveChanges();
+            }
         }
     }
 }
