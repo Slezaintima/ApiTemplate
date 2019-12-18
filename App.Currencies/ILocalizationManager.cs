acd0dba068dff54fe13d4b83918cabb1ab33af03
Konstantin Melnykov <kmelnykov11@gmail.com>
2019-12-18 22:34:40 +0200

    lab 5 implementation

---
+++ App.Currencies/ILocalizationManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace App.Currencies
+{
+    public interface ILocalizationManager
+    {
+        string GetResource(string key);
+        string GetResource(string key, CultureInfo culture);
+    }
+}
